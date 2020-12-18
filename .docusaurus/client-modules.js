bc74b1940efaf5327533c56485ed54bc8a71ccdf
spewil <spencer@spewil.com>
2020-12-18 11:07:48 -0500

    Remove build and docusaurus folders

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("C:\\Users\\zanem\\bonsai-rx-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\zanem\\bonsai-rx-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\zanem\\bonsai-rx-docs\\node_modules\\remark-admonitions\\styles\\infima.css"),
-  require("C:\\Users\\zanem\\bonsai-rx-docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\zanem\\bonsai-rx-docs\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
-  require("C:\\Users\\zanem\\bonsai-rx-docs\\src\\css\\custom.css"),
-];
