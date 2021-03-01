40ef3bd3295760ec1400f12795fcd3ff83e5159e
Spencer Wilson <spencer.wilson@ucl.ac.uk>
2021-03-01 14:18:45 +0000

    Update algolia to reflect repo name change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,7 +112,7 @@ module.exports = {
   // algolia search
   algolia: {
     apiKey: '1d5169d357726e74810feff638fc4978',
-    indexName: 'bonsai-rx',
+    indexName: 'docs',
 
     // // Optional: see doc section bellow
     // contextualSearch: true,
